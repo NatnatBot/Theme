0b96727ffe98770d3dba12ab7059a57ac4c68cd1
NatnatBot <ajdin291@hotmail.com>
2015-08-29 12:40:58 +0200

    Create PlugCubed.php

---
+++ php/PlugCubed.php
@@ -0,0 +1,135 @@
+{
+    "css": {
+        "font": [
+        ],
+        "import": [
+        ],
+        "rule": {
+             "#footer": {
+                "background": "linear-gradient(to bottom,#180000   0,#180000  90%)"
+            },
+            "#playlist-button": {
+                "background": "linear-gradient(to bottom,#FF5CDE 0,#5700FF 100%)"
+            },
+            "#header": {
+                "background": "linear-gradient(to bottom,#000000 0,#000000 90%)"
+            },
+            "#store-button": {
+                "background": "linear-gradient(to bottom,#FF0000 0,#A00000 100%)"
+            },
+            "#plug-dj": {
+                "background": "linear-gradient(to bottom,#0268AC 0,#02225C 100%)"
+            },
+            ".header-panel-button.selected": {
+                "background": "linear-gradient(to bottom,rgba(255, 255, 255, 0.25) 0,rgba(255, 255, 255, 0.06) 100%)"
+            },
+            "#plug-menu": {
+                "background": "rgba(40, 44, 53, 0.94) !important"
+            },
+            "#plug-menu .legal": {
+                "background": "rgba(68, 74, 89, 0.46) !important"
+            },
+            "#settings": {
+                "background": "rgba(10, 10, 10, 0.89) !important"
+            },
+            "#settings .header": {
+                "background": "rgba(28, 31, 37, 0.78) !important"
+            },
+            "#chat": {
+                "background": "linear-gradient(to bottom,#000000 0,#000000 90%)"
+            },
+            "#dj-button": {
+                "background": "rgba(40, 44, 53, 0.75) !important"
+            },
+            "#dj-button .left": {
+                "background": "linear-gradient(to bottom,rgba(0, 181, 230, 0.75) 0,rgba(0, 156, 221, 0.75) 100%)"
+            },
+            "#playlist-panel": {
+                "background": "rgba(17, 19, 23, 0.9)"
+            },
+            "#playlist-menu": {
+                "background": "rgba(28, 31, 37, 0.67)"
+            },
+            "#search": {
+                "background": "rgba(50, 55, 66, 0.58)"
+            },
+            "#search-bar": {
+                "background": "rgba(17, 19, 23, 0.77)"
+            },
+            "#filter-bar": {
+                "background": "rgba(17, 19, 23, 0.77)"
+            },
+            "#playlist-edit-button": {
+                "background": "rgba(85, 93, 112, 0.75)"
+            },
+            "#playlist-shuffle-button": {
+                "background": "rgba(68, 74, 89, 0.75)"
+            },
+            "#playlist-delete-button": {
+                "background": "rgba(50, 55, 66, 0.75)"
+            },
+            "#user-panel .score": {
+                "background": "rgba(28, 31, 37, 0.59)"
+            },
+            "#dj-button.is-locked .left,#dj-button.is-full .left,#vote .crowd-response.disabled .bottom": {
+                "background": "rgba(68, 74, 89, 0.75)"
+            },
+            "#dj-button.is-quit .left,#dj-button.is-leave .left,#playlist-delete-button:hover": {
+                "background": "linear-gradient(to bottom,rgba(244, 107, 64, 0.74) 0,rgba(240, 79, 48, 0.75) 100%)"
+            },
+            "#chat .message:nth-child(2n+1),#chat .mention:nth-child(2n+1),#chat .skip:nth-child(2n+1),#chat .moderation:nth-child(2n+1),#chat .emote:nth-child(2n+1),#chat .update:nth-child(2n+1),#chat .welcome:nth-child(2n+1)": {
+                "background-color": "rgba(17, 19, 23, 0.75)"
+            },
+            "#chat .emote.from-4573622": {
+                "border-left": "#ac76ff 3px solid",
+                "background-color": "rgba(24, 0, 38, 0.9) !important"
+            
+            },
+            ".emote.from-4573622 .from.staff.clickable": {
+                "display": "none"
+            },
+            ".emote.from-4573622 .icon-chat-manager": {
+                "display": "none"
+            },
+            "#chat .emote.from-4573622 .from": {
+                 "color": "#ac76ff !important"
+            },
+            "#chat .emote": {
+                "border-left": "rgba(174, 69, 189, 1) 3px solid !important",
+                "background-color": "rgba(190, 28, 213, 0.22) !important"
+            },
+            "#chat-messages .emote .icon": {
+                "left": "4px"
+            },
+            "#chat .emote .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff"
+            },
+             "#chat .emote.from-4573622 .text": {
+                "font-style": "normal",
+                "font-weight": "bold",
+                "color": "#ac76ff"
+            }
+              
+        }
+
+    },
+    "images": {
+        "background": "http://i.imgur.com/M4Z45oA.png",
+        "booth": "",
+        "playback": "http://i.imgur.com/dwBIvBo.jpg",
+    "icons":{
+         "bouncer":"",
+         "cohost":"",
+         "host":"",
+         "manager":"",
+         "residentdj":""
+      }
+    },
+    "rules": {
+        "allowAutorespond": "true",
+        "allowAutowoot": "true",
+	    "allowAutojoin": "true"
+    }
+}
